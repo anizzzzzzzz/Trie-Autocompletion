630492947776827af4b901f82e0c1b5256f044bc
aniz <anish.maharjan@infotmt.com>
2019-12-17 17:04:31 +0545

    added timer for typing in text box while fetching data

--- src/main/resources/static/js/autocomplete.js
+++ src/main/resources/static/js/autocomplete.js
@@ -1,15 +1,27 @@
-/*var typingTimer;                //timer identifier
-var doneTypingInterval = 2000;  //time in ms (5 seconds)
+var typingTimerFirst;                //timer identifier
+var typingTimerSecond;                //timer identifier
+var doneTypingInterval = 1500;  //time in ms (1.5 seconds)
 
-$('#text-field').keyup(function () {
-    clearTimeout(typingTimer);
-    if($('#text-field').val()){
-        typingTimer = setTimeout(autoComplete, doneTypingInterval);
-    }
-});*/
+$('#text-field-first').keyup(function () {
+    clearTimeout(typingTimerFirst);
+    typingTimerFirst = setTimeout(autocomplete_first, doneTypingInterval);
+});
+
+$('#text-field-second').keyup(function () {
+    clearTimeout(typingTimerSecond);
+    typingTimerSecond = setTimeout(autocomplete_second, doneTypingInterval);
+});
+
+function autocomplete_first() {
+    autoComplete('first');
+}
+
+function autocomplete_second() {
+    autoComplete('second');
+}
 
 function autoComplete(version) {
-    var text = document.getElementById("text-field").value;
+    var text = document.getElementById("text-field-"+version).value;
     var search = {skill:text};
     document.getElementById("suggestion-table").innerHTML = '';
     if(version === 'first')
